ae0ae984221c52eba8467aa25ca93c5c5d82b3fc
Gabriel Santana <gabriellima36716@gmail.com>
2022-07-17 22:32:53 -0300

    ok

--- app.py
+++ app.py
@@ -1,12 +1,139 @@
-from flask import (Flask, Blueprint, render_template, request)
+from flask import (Flask, Blueprint, render_template, request, session)
 from http import server
 import smtplib
-import database
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 bp = Blueprint('app', __name__)
+
+user = 'gnstrfnl' 
+database = 'gnstrfnl'
+password = '6EzO7_Wb5AaCkQ6fPbVIL5sSyM6YYbSd' 
+host = 'kesavan.db.elephantsql.com' 
+
+app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{user}:{password}@{host}/{database}' 
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = 'secreta'
+db = SQLAlchemy(app)
+
+
+#MODELO DO USUÁRIO
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name_ = db.Column(db.String(60), nullable = False)
+    birthday = db.Column(db.Date, nullable = False)
+    password = db.Column(db.String(10), nullable = False)
+    role = db.Column(db.String(25), nullable = False)
+    email = db.Column(db.String(120), nullable = False)
+    phone_number = db.Column(db.String(25), nullable = False)
+
+    def __init__(self, name_, birthday, password, role, email, phone_number):
+        self.name_ = name_
+        self.birthday = birthday
+        self.password = password
+        self.role = role
+        self.email = email
+        self.phone_number = phone_number
+
+#MODELO DO IMÓVEL
+class Imovel(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    status_id = db.Column(db.Integer, db.ForeignKey('status.id'))
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'))
+    prop_state_id = db.Column(db.Integer, db.ForeignKey('prop_state.id'))
+    space_id_id = db.Column(db.Integer, db.ForeignKey('space.id'))
+    banheiro = db.Column(db.Integer, nullable = False)
+    quarto = db.Column(db.Integer, nullable = False)
+    cidade = db.Column(db.String(25), nullable = False)
+    bairro = db.Column(db.String(25), nullable = False)
+    views = db.Column(db.Integer, nullable = False)
+    data_cadastro = db.Column(db.DateTime, nullable = False)
+    price = db.Column(db.Integer, nullable = False)
+
+    def __init__(self, user_id, status_id, category_id, prop_state_id, space_id_id, banheiro, quarto, cidade, bairro, views, data_cadastro, price):
+        self.user_id = user_id
+        self.status_id = status_id
+        self.category_id = category_id
+        self.prop_state_id = prop_state_id
+        self.space_id_id = space_id_id
+        self.banheiro = banheiro
+        self.quarto = quarto
+        self.cidade = cidade
+        self.bairro = bairro
+        self.views = views
+        self.data_cadastro = data_cadastro
+        self.price = price
+
+    @staticmethod
+    def read_all_imovel():
+        return Imovel.query.join(Img_imovel, Imovel.id == Img_imovel.imovel_id).order_by(Imovel.price)
+
+#MODELO DAS CATEGORIAS
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    category_name = db.Column(db.String(60), nullable = False)
+
+#MODELO DOS ESPAÇOS
+class Space(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    space_name = db.Column(db.String(60), nullable = False) 
+
+#MODELO DO ESTADO DA PROPRIEDADE
+class Prop_state(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    prop_state_name = db.Column(db.String(60), nullable = False)
+
+#MODELO DE STATUS DO IMÓVEL
+class Status(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    status_name = db.Column(db.String(60), nullable = False)
+
+#MODELO DAS FOTOS DOS IMOVEIS
+class Img_imovel(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    imovel_id = db.Column(db.Integer, db.ForeignKey('imovel.id'))
+    img_url = db.Column(db.String(1000), nullable = False)
+
+    def __init__(self, imovel_id, img_url):
+        self.imovel_id = imovel_id
+        self.img_url = img_url
+
+#MODELO DAS FOTOS DO USUÁRIO
+class Img_user(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    img_url = db.Column(db.String(1000), nullable = False)
+
+
+
+
+    @staticmethod
+    def read_all_img_imovel():
+        return Img_imovel.query.order_by(Img_imovel.imovel_id).all()
+    
+    @staticmethod
+    def read_single(pacientes_id):
+        return User.query.get(pacientes_id)
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self, new_data):
+        self.name_ = new_data.name_
+        self.idade = new_data.idade
+        self.estado = new_data.estado
+        self.sexo = new_data.sexo
+        self.dose = new_data.dose
+        self.save()
+
+    def delete(self):
+        db.session.delete(self) 
+        db.session.commit()
+
 
 def send_email(name, email, phone):
     host = "smtp.office365.com"
@@ -39,14 +166,10 @@ def send_email(name, email, phone):
 
 
 
-@app.route('/', methods = ('GET', 'POST'))
+@app.route('/')
 def index():
-    nome_cliente = None
-    if request.method =='POST':
-        form = request.form
-        send_email(form['nome'], form['email'], form['phone'])
-        nome_cliente = form['nome']
-    return render_template('index.html', nome_cliente = nome_cliente)
+    imoveis = Imovel.read_all_imovel()
+    return render_template('index.html', lista_imoveis = imoveis)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- query.sql
+++ query.sql
@@ -16,6 +16,31 @@ CREATE TABLE category(
     category_name VARCHAR(60) NOT NULL
 );
 
+insert into img_user (user_id, img_url)
+values (2, 'https://lh3.googleusercontent.com/Occzoo2LNM_
+iedcllpD1TlI2ag667HoMmvAxD41XZNkznD8lQXJr3NiVOqlO5mFOqp66DWAfkori_
+8FVaBp1D9ZUdYUxkyyq19d-D7ftTV0zPJKiytCAovxSbA7-jhbBkzcfiJUYkjtIGb2mG
+95KM87ihw1esvA84nBllEKS7g0s3azNDN4bTP35aD2Vcpn1eLM5pJtduW-gi09TQ2YUjk
+gVqkrECzUevbusFwVzehcrknme3HmR7eqUmTLLEgccbOUON3nwC1fiTrtRYqEglEwQHOE
+oLNEmK63tWHEk2xA2GfBcClOFk6KvFUynGHz7mq9-tYKoYP5fQ7Nantz0wsG6ysYsqVB
+E0XZTHg-eXDOwPvbmO17aSk1pWH1SOlv5kMR_gWGRNtN2CyOqVMVxTPEWtBlM-tEXa_E_
+3pHEuERSX_yGN8fBsqtruV2p715WtPG3-zhE-gb76Kf_TPZiSNcgIXkhH-B2DnpxxdPf
+RPNRiATuSnHW_ZVJ7xyWQ-KhZ6DUXCAcpeqiXNHRmgquilqemtYYxwuBTrw-agW6MZVY-
+LYCrlN_SrNf3lddGffQwqmelfXnXoOEf1rev7VZ9ekOcotnaD1vVPiitYP2eoGO2_WJrt
+EPshNGWcr_kin8LAspvcnMdhcTEC5FoGECe_Q26-aYA5AaftY93c5mirohSFvmebSU1kb
+aOlNpVPa17Cc5pBzDSMubYWlzf2J1ZTcnkTEgTnn9IaL38AFrGP003e1jg9DJR94U7ow=w
+1348-h903-no?authuser=0')
+
+insert into imovel (user_id, status_id, category_id, prop_state_id,
+space_id_id, banheiro, quarto, cidade, bairro, views, data_cadastro,
+price)
+values (2, 1, 3, 2, 1, 2, 3, 'Porto Seguro', 'Village II', 0,
+'07/17/2022', 365000);
+
+insert into users (name, birthday, password, role, email, phone_number) 
+values ('Victor Moura', '01/12/1999', 'regina1962', 'Corretor imobiliário', 
+'vitors-dm@hotmail.com', '73991172026');
+
 insert into users (category_name) 
 values ('Admin');
 insert into users (category_name) 
@@ -23,3 +48,7 @@ values ('Corretor');
 insert into users (category_name) 
 values ('Estagiário');
 
+insert into imovel (user_id, status_id, category_id,
+prop_state_id, space_id, banheiro, quarto, cidade,
+visualizacoes, data_cadastro, price)
+values (2, );
